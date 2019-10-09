af5826ca3b25214b559a09b3a91a4af849c89fd1
Hannah Bingham <binghan.hb@gmail.com>
2019-10-09 09:19:44 -0500

    Added migration and created data base and table for movies.

--- WebAPISample/Models/Movie.cs
+++ WebAPISample/Models/Movie.cs
@@ -8,7 +8,9 @@ namespace WebAPISample.Models
     public class Movie
     {
         public int MovieId { get; set; }
+        public string Genre { get; set; }
         public string Title { get; set; }
         public string Director { get; set; }
+        
     }
 }
