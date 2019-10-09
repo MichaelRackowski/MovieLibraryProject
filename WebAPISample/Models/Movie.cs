42b4d1ed4802d87ebf7139871fe88203865b77d8
Hannah Bingham <binghan.hb@gmail.com>
2019-10-09 09:31:10 -0500

    merge conflict resolved

--- WebAPISample/Models/Movie.cs
+++ WebAPISample/Models/Movie.cs
@@ -10,12 +10,9 @@ namespace WebAPISample.Models
         public int MovieId { get; set; }
         public string Genre { get; set; }
         public string Title { get; set; }
-<<<<<<< HEAD
-        public string Genre { get; set; }
         public string DirectorName { get; set; }
-=======
-        public string Director { get; set; }
+
         
->>>>>>> af5826ca3b25214b559a09b3a91a4af849c89fd1
+
     }
 }
