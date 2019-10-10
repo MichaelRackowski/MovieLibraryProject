4ee60b178d80e372856fedf16261c8f4e8c609ba
Hannah Bingham <binghan.hb@gmail.com>
2019-10-10 14:56:44 -0500

    added updatemovie function. Content  doesnt  even make it to the ajaxs portion. API is never entered for the updated details.

--- Client/app.js
+++ Client/app.js
@@ -13,7 +13,7 @@
             contentType: 'application/json',
             data: JSON.stringify(dict),
             success: function( data, textStatus, jQxhr ){
-                console.log("success"),
+                alert("You have successfully added a movie! Click the Generate Movie List button to reload the movies."),
                 $('#response pre').html( data );
             },
             error: function( jqXhr, textStatus, errorThrown ){
@@ -62,9 +62,44 @@ function GetSingleMovie(id){
             stuff+="<td>" +data.Title +"</td>";
             stuff+="<td>"+data.DirectorName+"</td>";
             stuff+="<td>"+data.Genre+"</td>";
-            stuff+="<td><button onclick='UpdateMovie(" +value.MovieId+ ")'>Update</button>";
             stuff+="</tr>";
             $('#MovieBody').html(stuff);
         }
     })
+    var editForm="";
+    editForm+=" <input type='text'id='title'name='title'placeholder='Title'/>";
+    editForm+=" <input type='text'id='director'name='director'placeholder='DirectorName'/>";
+    editForm+="<input type='text'id='genre' name='genre' placeholder='Genre'/>";
+    editForm+="<button onclick='UpdateMovie("+id+")'>Update</button>";
+    $('#edit').append(editForm);
+    
 }
+
+function UpdateMovie(id){
+    let editMovie={
+     Title: document.getElementById('title').value,
+     DirectorName:document.getElementById('director').value,
+     Genre:document.getElementById('genre').value,
+    }
+    $.ajax({
+        url:'https://localhost:44352/api/movie/'+id,
+        dataType: 'json',
+        contentType:'application/json',
+        type: 'Put',
+        data:JSON.stringify(editMovie),
+        success:function(data,textStatus,jQxhr){
+         console.log("YEAH");
+          },
+        error:function(jqXhr,textStatus,errorThrown){
+            console.log(errorThrown);
+            console.log(editMovie);
+        }
+    });
+
+
+}
+
+//  <input type="text"name="title" placeholder="Title"/>
+//  <input type="text"name="director"placeholder="DirectorName"/>
+//  <input type="text"name="genre"placeholder="Genre"/>
+//  <button type="submit">Update</button> 
